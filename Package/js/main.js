10ba387a3a31917a130ac2fe47f2f5b335fc247e
marek4882 <cwioro200@gmail.com>
2024-08-26 15:12:41 +0200

    .

--- Package/js/main.js
+++ Package/js/main.js
@@ -344,7 +344,7 @@ function showPackageDetails(pkg) {
                         </ul>
                         <a href="" class="btn btn--block ${
                           pkg.btn
-                        }">Zamów Pakiet</a>
+                        }"><i class="ph ph-basket"></i>Zamów Pakiet</a>
                       </div>
                     </div>
                   </div>
